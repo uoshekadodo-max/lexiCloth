34fcd1aecb1e82c47b22c60de993295decf306f7
Osheka <udodoosheka@gmail.com>
2026-08-30 13:38:11 +0100

    cart changing

--- js/pages/cartPage.js
+++ js/pages/cartPage.js
@@ -46,7 +46,7 @@ function renderCart(cart) {
 
                 <p>You haven't added any products yet.</p>
 
-                <a href="/html/shop.html" class="continue-shopping">
+                <a href="#" class="continue-shopping">
 
                     Continue Shopping
 
@@ -56,7 +56,12 @@ function renderCart(cart) {
 
         `;
 
-        
+       const continueShopping =
+    document.getElementById("continue-shopping");
+
+if (continueShopping) {
+    continueShopping.href = CONFIG.PATHS.SHOP;
+}
 
       
 
